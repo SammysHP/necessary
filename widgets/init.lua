2f4d34c4d4699bc82b518cb9f53593d8ce5901ed
SammysHP <sven@sammyshp.de>
2017-02-03 18:14:16 +0100

    Add taginfo widget

--- widgets/init.lua
+++ widgets/init.lua
@@ -1,5 +1,6 @@
 return {
     alsavolume = require("necessary.widgets.alsavolume"),
     mpd = require("necessary.widgets.mpd"),
+    taginfo = require("necessary.widgets.taginfo"),
     smapibat = require("necessary.widgets.smapibat"),
 }
---
+++ widgets/taginfo.lua
@@ -0,0 +1,26 @@
+local textbox = require("wibox.widget.textbox")
+local awful = require("awful")
+
+local taginfo = {}
+
+local function refresh(w)
+    return function (tag)
+        if not tag then return end
+        w:set_text(tag.master_count .. "/" .. tag.column_count .. " ")
+    end
+end
+
+function taginfo.new(screen)
+    local w = textbox()
+    local callback = refresh(w)
+
+    callback(screen.selected_tag)
+
+    awful.tag.attached_connect_signal(screen, "property::master_count", callback)
+    awful.tag.attached_connect_signal(screen, "property::column_count", callback)
+    awful.tag.attached_connect_signal(screen, "property::selected", callback)
+
+    return { widget = w }
+end
+
+return taginfo
